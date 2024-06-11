698baa3924e022711d56ca8d1ef36c471bb193e8
Ansel Santosa <568242+anstosa@users.noreply.github.com>
2024-06-11 11:35:36 -0700

    Initial build

---
+++ dist/farmrpg-farmhand.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name Farm RPG Farmhand
+// @description Your helper around the RPG Farm
+// @version 1.0.0
+// @author Ansel Santosa <568242+anstosa@users.noreply.github.com>
+// @match https://farmrpg.com/*
+// @grant GM.getValue
+// @grant GM.setValue
+// @icon https://www.google.com/s2/favicons?sz=64&domain=farmrpg.com
+// ==/UserScript==
---
+++ dist/farmrpg-farmhand.proxy.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name Farm RPG Farmhand
+// @description Your helper around the RPG Farm
+// @version 1.0.0
+// @author Ansel Santosa <568242+anstosa@users.noreply.github.com>
+// @match https://farmrpg.com/*
+// @grant GM.getValue
+// @grant GM.setValue
+// @icon https://www.google.com/s2/favicons?sz=64&domain=farmrpg.com
+// @require http://localhost:3000/farmrpg-farmhand.user.js
+// ==/UserScript==
